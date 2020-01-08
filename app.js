ada4eb773f4154ed31c227d2092941e5e02d9ec7
Odutusin moses <48915357+thebolarin@users.noreply.github.com>
2020-01-08 07:41:08 -1200

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ sequelize
   })
   .then(user => {
     if (!user) {
-      return User.create({ name: 'Max', email: 'test@test.com' });
+      return User.create({ name: '', email: '' });
     }
     return user;
   })
